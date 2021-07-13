b7e410b789a165ac84524562c57c1e014cdab14c
Alwayspkr <alwayspawan148@gmail.com>
2021-07-13 11:20:48 +0530

    first commit

---
+++ src/main/java/com/pktech/InsuranceBackendProjectApplication.java
@@ -0,0 +1,16 @@
+package com.pktech;
+
+import javax.persistence.Table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class InsuranceBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceBackendProjectApplication.class, args);
+	}
+
+}
